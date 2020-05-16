e12f1bb2f50a2ed0a5010e32575f28dcb5a2530a
kuojed <kuojed@gmail.com>
2020-05-16 16:57:21 +0800

    Initial Commit

---
+++ stopWatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stopWatch
+//
+//  Created by 郭冠杰 on 2020/5/16.
+//  Copyright © 2020 郭冠杰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
